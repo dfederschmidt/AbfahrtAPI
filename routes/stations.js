c9ad0666617f2650ec3be42cd430397a62969e4d
Daniel <daniel.federschmidt@googlemail.com>
2015-01-16 00:54:20 +0100

    Version 0.1.0

---
+++ routes/stations.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var stations = require('../public/resources/stations.js');
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+/* GET stations listing. */
+router.get('/', function(req, res) {
+  res.send(stations);
+});
+router.get('/:id', function(req, res) {
+  for(var i = 0; i < stations.length;i++){
+    if(stations[i].id == req.params.id){
+      res.send(stations[i]);
+    }
+  }
+
+});
+/* GET departure information on a station */
+router.get('/:id/departures', function(req, res) {
+  for(var i = 0; i < stations.length;i++){
+    if(stations[i].id == req.params.id){
+      request('http://www.swtue.de/abfahrt/?halt=' + req.params.id, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var departures = []
+          console.log(body);
+          $ = cheerio.load(body);
+
+          for(var i = 0; i < $('td[class=linie]').length;i++) {
+            var connection = {};
+            connection.line = $('td[class=linie]')[i].children[0].data.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/g,"");
+            connection.direction = $('td[class=richtung]')[i].children[0].data.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/g,"");
+            connection.departure = $('td[class=abfahrt]')[i].children[0].data.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/g,"");
+            departures.push(connection);
+          }
+          res.send(departures);
+        }
+      })
+
+
+    }
+  }
+
+});
+
+module.exports = router;
